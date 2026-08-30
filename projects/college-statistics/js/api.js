12a4102e4d5f6cb5a4a58899e5362393d1298b6d
xkcao <xkcao1984@gmail.com>
2026-08-30 12:39:37 -0700

    Simplify College Statistics data loading

--- projects/college-statistics/js/api.js
+++ projects/college-statistics/js/api.js
@@ -1,4 +1,4 @@
-const DATA_FILES = ["universities","majors","university-major","rankings","tuition","employment","salaries","admissions","metadata"];
+const DATA_FILES = ["universities","majors","university-major","tuition","salaries","admissions","metadata"];
 
 async function loadJson(name){
   const response = await fetch(`data/${name}.json`, {cache:"no-cache"});
@@ -14,9 +14,7 @@ export async function loadData(){
 export function buildRecords(data){
   const universities = new Map(data.universities.map(item => [item.university_id,item]));
   const majors = new Map(data.majors.map(item => [item.major_id,item]));
-  const keyed = list => new Map(list.map(item => [`${item.university_id}|${item.major_id ?? ""}`,item]));
-  const salaries = keyed(data.salaries);
-  const employment = keyed(data.employment);
+  const salaries = new Map(data.salaries.map(item => [`${item.university_id}|${item.major_id}`,item]));
   const tuition = new Map(data.tuition.map(item => [item.university_id,item]));
   const admissions = new Map(data.admissions.map(item => [item.university_id,item]));
 
@@ -24,6 +22,6 @@ export function buildRecords(data){
     const university = universities.get(rel.university_id);
     const major = majors.get(rel.major_id);
     const pairKey = `${rel.university_id}|${rel.major_id}`;
-    return {university,major,relationship:rel,salary:salaries.get(pairKey),employment:employment.get(pairKey),tuition:tuition.get(rel.university_id),admissions:admissions.get(rel.university_id)};
+    return {university,major,relationship:rel,salary:salaries.get(pairKey),tuition:tuition.get(rel.university_id),admissions:admissions.get(rel.university_id)};
   }).filter(record => record.university && record.major);
 }
