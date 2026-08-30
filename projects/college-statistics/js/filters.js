6785375d6835b4e03ea05df23f7f6dbed8b38bb1
xkcao <xkcao1984@gmail.com>
2026-08-30 11:49:35 -0700

    Add university filter

--- projects/college-statistics/js/filters.js
+++ projects/college-statistics/js/filters.js
@@ -1,9 +1,10 @@
 import {matchesSearch} from "./search.js";
 
-export function applyFilters(records,{query,major,maxTuition}){
+export function applyFilters(records,{query,university,major,maxTuition}){
   return records.filter(record => {
     const tuition=record.tuition?.tuition;
     return matchesSearch(record,query) &&
+      (university==="all" || record.university.university_id===university) &&
       (major==="all" || record.major.major_id===major) &&
       (maxTuition==="all" || (Number.isFinite(tuition) && tuition<=Number(maxTuition)));
   });
