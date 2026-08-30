1e27f46049b1893cf431d659c0d72ce7b23d650a
xkcao <xkcao1984@gmail.com>
2026-08-30 11:37:13 -0700

    Remove unsupported employment filter

--- projects/college-statistics/js/filters.js
+++ projects/college-statistics/js/filters.js
@@ -1,12 +1,10 @@
 import {matchesSearch} from "./search.js";
 
-export function applyFilters(records,{query,major,maxTuition,minEmployment}){
+export function applyFilters(records,{query,major,maxTuition}){
   return records.filter(record => {
     const tuition=record.tuition?.tuition;
-    const employment=record.employment?.employment_rate;
     return matchesSearch(record,query) &&
       (major==="all" || record.major.major_id===major) &&
-      (maxTuition==="all" || (Number.isFinite(tuition) && tuition<=Number(maxTuition))) &&
-      (!Number(minEmployment) || (Number.isFinite(employment) && employment>=Number(minEmployment)));
+      (maxTuition==="all" || (Number.isFinite(tuition) && tuition<=Number(maxTuition)));
   });
 }
