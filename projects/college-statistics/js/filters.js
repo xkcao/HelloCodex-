69e86222099d973aefcd841b28eb54ab5ab8bd81
xkcao <xkcao1984@gmail.com>
2026-09-05 17:02:02 -0700

    Add school location and type filters

--- projects/college-statistics/js/filters.js
+++ projects/college-statistics/js/filters.js
@@ -1,11 +1,14 @@
 import {matchesSearch} from "./search.js";
 
-export function applyFilters(records,{query,university,major,maxTuition}){
+export function applyFilters(records,filters){
+  const {query,university,major,state,institutionType,maxTuition}=filters;
   return records.filter(record => {
     const tuition=record.tuition?.tuition;
     return matchesSearch(record,query) &&
       (university==="all" || record.university.university_id===university) &&
       (major==="all" || record.major.major_id===major) &&
+      (state==="all" || record.university.state===state) &&
+      (institutionType==="all" || record.university.type===institutionType) &&
       (maxTuition==="all" || (Number.isFinite(tuition) && tuition<=Number(maxTuition)));
   });
 }
